75f29edee15cec2403f21f08bbf3ca69c0d907ca
Minifyre <minifyre@gmail.com>
2018-12-22 16:04:01 -0600

    +input.blur

--- src/input.js
+++ src/input.js
@@ -7,6 +7,37 @@ input.backOrOpts=function(state,evt)
 {
 	(state.view.path.length?input.back:input.opts)(state,evt)
 }
+input.blur=function(state,{target})
+{
+	const
+	{innerText:text}=target,
+	{id}=target.parentElement
+
+	if(util.empty(text))
+	{
+		if(util.empty(state.file.data[id].text)) logic.remove(state,id)
+		else target.innerHTML=state.file.data[id].text
+		//previous line is necessary as the text has not changed 
+			//in the virtual dom & so it will not get re-rendered
+	}
+	else
+	{
+		const
+		[val,...vals]=util.txt2txts(text),
+		parentId=logic.parent(state,id),
+		childIndex=state.file.data[parentId].list.indexOf(id)
+	
+		target.innerHTML=val
+		logic.itemUpdate(state,id,{text:val})
+	
+		vals.forEach((text,i)=>logic.itemAdd(state,logic.item({text}),parentId,childIndex+i+1))	
+	}
+
+	logic.edit(state)
+
+	//if list is empty, go back (no bugs on empty index list)
+	if(!state.file.data[logic.listLowest(state)].list.length) logic.back(state)
+}
 input.delete=function(state)
 {
 	//remove deleted items from path
