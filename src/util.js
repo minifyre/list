b86230efa2470646a6a72e6666cfa5a9292c2957
Minifyre <minifyre@gmail.com>
2018-12-23 18:08:10 -0600

    +helper functions for new item input

--- src/util.js
+++ src/util.js
@@ -1,3 +1,29 @@
+util.txt2txts=function(txt)
+{
+	return txt.split(config.newline)
+			.filter(x=>x.length)
+			.map(util.txtRepeat)
+			.reduce(util.flatten,[])
+}
+util.txtRepeat=function(txt)
+{
+	const repeat=/X:\d+-\d+/
+
+	if(!txt.match(repeat)) return [txt]
+
+	const
+	[replace]=txt.match(repeat),//X:11-2
+	[min,max]=replace.split(':')[1]//11-2
+			.split('-')//[11','2']
+			.map(d=>parseInt(d))//[11,2]
+			.sort((a,b)=>a-b)//[2,11]//sort() will keep 11 in front
+
+	return Array(max-min+1)
+			.fill(min)
+			.map((d,i)=>d+i)
+			.map(d=>txt.replace(repeat,d))
+}
+//generic
 util.empty=txt=>!txt.replace(/\s/g,'').length
 //useful when rerendering before array length is updated (e.g. pop/splice)
 util.mapEmpty=(arr,fn)=>arr.filter(x=>!!x).map(fn)
