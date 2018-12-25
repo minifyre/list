fa19b379bd89d2b3d29498e943359c1ed410725a
Minifyre <minifyre@gmail.com>
2018-12-24 19:32:50 -0600

    +more-readable formatting

--- src/output.js
+++ src/output.js
@@ -26,7 +26,8 @@ output.render=function(state)
 	const
 	move=state.view.move.filter(x=>!!x),
 	[pointerup,mkList]=[input,output.list].map(fn=>util.curry(fn,state)),
-	[path,filter]=move.length?[move,id=>!state.view.selected.includes(id)]:[state.view.path,()=>true],
+	[path,filter]=	move.length?[move,id=>!state.view.selected.includes(id)]:
+					[state.view.path,()=>true],
 	lists=path.filter(x=>!!x).map(util.curry(mkList,filter)),
 	main=v('main',{data:{view:state.view.layout},on:{pointerup}},...lists)
 
